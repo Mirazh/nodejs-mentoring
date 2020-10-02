cec1ecad71a53e275d0ef3ad414cbb5254b5d7eb
Denis Sidorenko <denis_sidorenko@epam.com>
2020-10-02 14:27:43 +0300

    task-5: add custom logger

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,22 @@ import express from 'express';
 import { apiRouter } from './routes';
 import { connectToDB } from './config/dbConnect';
 
-import { UserGroupService } from "./components/userGroup";
-import { Service as GroupService } from "./components/group";
+import { UserGroupService } from './components/userGroup';
+import { Service as GroupService } from './components/group';
 
 const app: express.Application = express();
 const PORT = 3000;
+const customLogger = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log('HTTP method:', req.method);
+    console.log('Request body:', req.body);
+    console.log('Request params:', req.params);
+    console.log('Request query:', req.query);
+
+    next();
+};
 
 app.use(express.json());
+app.use(customLogger);
 app.use(apiRouter);
 
 connectToDB().then(() => app.listen(PORT, async () => {
