4e5ace00584d5159eaa106bf491077dc7e66bf5e
Denis Sidorenko <denis_sidorenko@epam.com>
2020-10-30 15:35:58 +0300

    task-7: add .env

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,6 +1,13 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/node_mentoring');
+dotenv.config();
+
+// @ts-ignore
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+});
 
 export const connectToDB = async () => {
     try {
